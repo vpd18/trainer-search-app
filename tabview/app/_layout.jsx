f9e1ed312942393d830249f45a6f04b1dfd0750d
vpd18 <2024is_vedpranavdas_d@nie.ac.in>
2025-11-18 16:30:30 +0530

    tried routing

---
+++ tabview/app/_layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function RootLayout() {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
